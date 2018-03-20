9a7f0ead1875bb0cfb9664933d70e075ee11e4ff
Joshua Hutchinson <jthutch94@gmail.com>
2018-03-20 16:33:57 -0400

    challenge complete

--- routes/notes.js
+++ routes/notes.js
@@ -4,47 +4,117 @@ const express = require('express');
 // Create an router instance (aka "mini-app")
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const Note = require('../models/note');
+
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' }, 
-    { id: 2, title: 'Temp 2' }, 
-    { id: 3, title: 'Temp 3' }
-  ]);
+  let filter = {};
+  let projection = {};
+  let sort = 'created';
 
+  if(searchTerm) {
+    filter.$text = { $search: searchTerm };
+    projection.score = {$meta: 'textScore'};
+    sort = projection;
+  }
+
+  Note.find(filter, projection)
+    .sort(sort)
+    .then(results=>{
+      res.json(results);
+    })
+    .catch(err =>{
+      next(err);
+    });
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
 
-  console.log('Get a Note');
-  res.json({ id: 2 });
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
 
+  Note.findById(id)
+    .then(result => {
+      if(result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2 });
+  if(!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
 
+  const newItem = { title, content};
+
+  Note.create(newItem)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(next);
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
 
-  console.log('Update a Note');
-  res.json({ id: 2 });
+  if(!title){
+    const err = new Error('Missing `title` from request body');
+    err.status = 400;
+    return next(err);
+  }
 
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is invalid');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateItem = { title, content};
+  const options = { new: true };
+
+  Note.findByIdAndUpdate(id, updateItem, options)
+    .then(result =>{
+      if(result){
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
 
-  console.log('Delete a Note');
-  res.status(204).end();
+  Note.findByIdAndRemove(id)
+    .then(result=>{
+      if(result){
+        res.status(204).end();
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 
 });
 
